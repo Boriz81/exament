edf60b845ec57d92d1ff6c88e65c77dd93a69a1a
boriz81 <blitzboriz@gmail.com>
2023-02-13 23:16:53 +0900

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        String input = scanner.nextLine();
+        String[] inArr = input.split(" ");
+        int count = 0;
+        System.out.println();
+        for (int x = 0;x < inArr.length; x++) {
+            System.out.println(inArr[x]);
+
+        }
+
+
+
+
+        //(1 <= fi <= 2*10^5);
+
+        System.out.println(2*10^5);
+        System.out.println("Hello world!");
+    }
+}
